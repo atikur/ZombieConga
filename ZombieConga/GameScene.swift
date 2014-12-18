43d5fdb6c5e0c5a11fe53d0f5d056e0ea28abd33
Atikur Rahman <atik.bd08@gmail.com>
2014-12-18 18:27:14 +0600

    add zombie sprite node

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -12,9 +12,15 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.whiteColor()
         
+        // add background sprite
         let background = SKSpriteNode(imageNamed: "background1")
         background.position = CGPointMake(size.width/2, size.height/2)
         background.zPosition = -1
         addChild(background)
+        
+        // add zombie
+        let zombie = SKSpriteNode(imageNamed: "zombie1")
+        zombie.position = CGPointMake(400, 400)
+        addChild(zombie)
     }
 }
