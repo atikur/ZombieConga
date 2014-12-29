6e8212f88bcb8b3fba3421a1fb5721ad2912c999
Atikur Rahman <atik.bd08@gmail.com>
2014-12-29 18:29:08 +0600

    Add MyUtils helper class

---
+++ ZombieConga/MyUtils.swift
@@ -0,0 +1,82 @@
+//
+//  MyUtils.swift
+//  ZombieConga
+//
+//  Created by Atikur Rahman on 12/29/14.
+//  Copyright (c) 2014 Atikur Rahman. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += (inout left: CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func -= (inout left: CGPoint, right: CGPoint) {
+    left = left - right
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func *= (inout left: CGPoint, right: CGPoint) {
+    left = left * right
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func *= (inout point: CGPoint, scalar: CGFloat) {
+    point = point * scalar
+}
+
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+func /= (inout left: CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+func /= (inout point: CGPoint, scalar: CGFloat) {
+    point = point / scalar
+}
+
+#if !(arch(x86_64) || arch(arm64))
+    func atan2(y: CGFloat, x: CGFloat) -> CGFloat {
+        return CGFloat(atan2f(Float(y), Float(x)))
+    }
+    
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrtf(Float(a)))
+    }
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+    
+    var angle: CGFloat {
+        return atan2(y, x)
+    }
+}
